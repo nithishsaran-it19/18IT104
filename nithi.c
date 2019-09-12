ace4a19c83901885064a39a3b87a2a97a9be92d4
nithishsaran-it19 <nithishsaran.it19@bitsathy.ac.in>
2019-09-12 22:21:49 +0530

    second commit

---
+++ nithi.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the three numbers");
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if(a>b&&a<c)
+{
+printf("a is bigger");
+}
+else if(b>c)
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}
+}
